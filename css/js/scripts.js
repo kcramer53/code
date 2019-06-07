1810e54377147d0c2383ba440302cef382551d98
Kirsten Putman <kirstenputman@Kirstens-MacBook-Pro.local>
2019-06-07 14:33:28 -0700

    jquery lost

--- css/js/scripts.js
+++ css/js/scripts.js
@@ -10,43 +10,28 @@
 
 	$(document).ready(function() {
 	  $("form").submit(function(event) {
-	    var favoriteCelebrity = $("#celebrity").val();
-	    var favoriteColor = $("input:radio[name=color]:checked").val();
-	    var favoriteFood = $("input:radio[name=food]:checked").val();
-	    var favoriteMovieTv = $("input:radio[name=movie-tv]:checked").val();
-	    var favoriteVacation = $("input:radio[name=vacation]:checked").val();
+	    var favoriteVacation = $("#vacation").val();
+	    var favoriteCusine = $("input:radio[name=cusine]:checked").val();
+	    var favoriteAccomodations = $("input:radio[name=accomodations]:checked").val();
+	    var favoriteMApproach = $("input:radio[name=approach]:checked").val();
+	    var favoriteRandom = $("input:radio[name=random]:checked").val();
 
 
-	    if (favoriteColor === "green" && favoriteFood === "sushi" && favoriteMovieTv === "notebook" && favoriteVacation === "bali") {
-	      $('#ryan').show();
-	      $('#woody, #kathy, #jamie, #no-match').hide();
-	      // $('#kathy').hide();
-	      // $('#jamie').hide();
-	      // $('#no-match').hide();
-	    } else if (favoriteColor === "red" && favoriteFood === "pizza" && favoriteMovieTv === "cheers" && favoriteVacation === "mexico") {
-	      $('#woody').show();
-	      $('#ryan').hide();
-	      $('#kathy').hide();
-	      $('#jamie').hide();
-	      $('#no-match').hide();
-	    } else if (favoriteColor === "blue" && favoriteFood === "cheeseburger" && favoriteMovieTv === "misery" && favoriteVacation === "switzerland") {
-	      $('#kathy').show();
-	      $('#ryan').hide();
-	      $('#woody').hide();
-	      $('#jamie').hide();
-	      $('#no-match').hide();
-	    } else if (favoriteColor === "purple" && favoriteFood === "fried-chicken" && favoriteMovieTv === "sense8" && favoriteVacation === "tahiti") {
-	      $('#jamie').show();
-	      $('#ryan').hide();
-	      $('#woody').hide();
-	      $('#kathy').hide();
-	      $('#no-match').hide();
+	    if (favoriteVacation === "tropics" && favoriteCusine === "seafood" && favoriteAccomodations === "youth hostel" && favoriteApproach === "visual concept" && favoriteRandom === "i am always on time")) {
+	      $('#ruby').show();
+	      $('#java, #c++, #python, #no-match').hide();
+	    } else if (favoriteVacation === "mountians" && favoriteCusine === "exotic" && favoriteAccomodations === "all inclusive" && favoriteApproach === "demonstration" && favoriteRandom === "i am the life of the party")) {
+        $('#java').show();
+        $('#ruby, #c++, #python, #no-match').hide();
+	    } else if (favoriteVacation === "dessert" && favoriteCusine === "traditional" &&favoriteAccomodations === "hotel" && favoriteApproach === "online" && favoriteRandom === "i never give up on true love")) {
+        $('#c++').show();
+        $('#java, #ruby, #python, #no-match').hide();
+	    } else if (favoriteVacation === "stacation" && favoriteCusine === "meat & taters" &&favoriteAccomodations === "tent" && favoriteApproach === "classroom" && favoriteRandom === "i will be rich some day")) {
+        $('#python').show();
+        $('#java, #c++, #ruby, #no-match').hide();
 	    } else {
-	      $('#no-match').show();
-	      $('#jamie').hide();
-	      $('#ryan').hide();
-	      $('#woody').hide();
-	      $('#kathy').hide();
+        $('#no-match').show();
+        $('#java, #c++, #python, #ruby').hide();
 	    }
 
 
